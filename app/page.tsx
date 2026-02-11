0e3b9576116ae0b6cf55680a56851b3230aa5b1e
Abhay Patel <patelabhay6980@gmail.com>
2026-02-11 19:13:58 +0530

    colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+# File: app/page.tsx
 'use client';
 
 import Calculator2D from '../components/Calculator2D';
@@ -9,8 +10,8 @@ export default function Home() {
       <div className="absolute inset-0 bg-grid opacity-20 pointer-events-none" />
 
       {/* Dynamic Background Blobs */}
-      <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-blue-600/20 rounded-full blur-[100px] animate-pulse" />
-      <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-purple-600/20 rounded-full blur-[100px] animate-pulse delay-1000" />
+      <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-green-600/20 rounded-full blur-[100px] animate-pulse" />
+      <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-yellow-500/20 rounded-full blur-[100px] animate-pulse delay-1000" />
 
       <Calculator2D />
 
